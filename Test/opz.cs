d3de2757157d04466bc0c310a2e193df39797057
Davide Russo <daviderusso1984@outlook.it>
2020-04-29 15:24:46 +0200

    first

---
+++ Test/opz.cs
@@ -0,0 +1,22 @@
+namespace Test
+{
+
+    public enum Directions
+    {
+        row, row_reverse, column, column_reverse
+    }
+
+    public enum Vertical_aligns
+    {
+        start, center, end, stretch, baseline
+    }
+
+    public enum Horizontal_alings
+    {
+        start, center, end, space_around, space_between
+    }
+
+    //
+
+
+}
